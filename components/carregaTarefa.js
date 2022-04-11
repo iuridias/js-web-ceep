8bea64a75a48d54fd4a3fe9c6f03c542bff40a51
Iuri Dias <iuridias.mw@gmail.com>
2022-04-11 00:28:36 -0300

    projeto final ceep

---
+++ components/carregaTarefa.js
@@ -0,0 +1,19 @@
+import { removeDatasRepetidas, ordenaDatas } from "../service/data.js";
+import { criaData } from "./criaData.js";
+
+export const carregaTarefa = () => {
+  const lista = document.querySelector('[data-list]');
+
+  const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+  lista.innerHTML = '';
+  
+  const datasUnicas = removeDatasRepetidas(tarefasCadastradas)
+
+  ordenaDatas(datasUnicas);
+
+  datasUnicas.forEach(dia => {
+    lista.appendChild(criaData(dia));
+
+  })
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# JS-WEB: Lista de tarefas
+Projeto em HTML e JS de uma lista de tarefas.
+
+
+Todo projeto foi criado com base nos seguintes curso da Alura:
+
+- JS na Web: Manipule o DOM com JavaScript
+- JS na Web: Armazenando dados no navegador
